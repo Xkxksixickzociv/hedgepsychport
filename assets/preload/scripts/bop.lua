4c08f398e55d19bf72b368a4911755123f6b5a28
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2023-01-01 15:34:48 +0200

    Add files via upload

---
+++ assets/preload/scripts/bop.lua
@@ -0,0 +1,6 @@
+function onUpdatePost()
+    setProperty('iconP1.origin.x',-60)
+    setProperty('iconP1.origin.y',-10)
+    setProperty('iconP2.origin.x',-20)
+    setProperty('iconP2.origin.y',-20)
+end
